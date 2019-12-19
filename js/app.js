a6d950fd1705d819a30113a9be207c63d41d2200
Juice Cx <juicecx@Juices-MacBook-Pro-2.local>
2019-12-19 15:15:18 -0600

    added Stewie in index.html and css

--- js/app.js
+++ js/app.js
@@ -2,22 +2,7 @@ console.log('tamagatchi mini game');
 
 
 
-* You must have a class for your Tamagotchi, a `game` object, and event listeners/handlers at the bottom that call methods in your `game` object.  There should be **no** other code in the global scope.
-* You must build incrementally and make commits as you go.  You will be expected to show a commit history that reflects taking careful measured steps and building one feature at a time. 
-  
-* Create a repo for your Tamagotchi pet on github.com and make the link available in your class Slack channel.
-
-* Create a `class` for your Tamagotchi.  The Tamagotchi should have the following properties:
-  * hunger (1-10 scale)
-  * sleepiness (1-10 scale)
-  * boredom (1-10 scale)
-  * age
-  * name
-
-
-
-
-class Tommy {
+class Tom {
 	constructors() {
 		this.name = name
 		this.age = age
@@ -29,7 +14,13 @@ class Tommy {
   }
 
 const game = {
+	name: '',
+	initiateTom() {
 
+	},
+	startGame() {
+
+	}
 }
 
 
