40f52263de9fc7cd8631b61596a69355a01166a4
Juice Cx <juicecx@Juices-MacBook-Pro-2.local>
2019-12-19 14:32:14 -0600

    added a form and button to enter tomagatchi name

---
+++ js/app.js
@@ -0,0 +1,36 @@
+console.log('tamagatchi mini game');
+
+
+
+* You must have a class for your Tamagotchi, a `game` object, and event listeners/handlers at the bottom that call methods in your `game` object.  There should be **no** other code in the global scope.
+* You must build incrementally and make commits as you go.  You will be expected to show a commit history that reflects taking careful measured steps and building one feature at a time. 
+  
+* Create a repo for your Tamagotchi pet on github.com and make the link available in your class Slack channel.
+
+* Create a `class` for your Tamagotchi.  The Tamagotchi should have the following properties:
+  * hunger (1-10 scale)
+  * sleepiness (1-10 scale)
+  * boredom (1-10 scale)
+  * age
+  * name
+
+
+
+
+class Tommy {
+	constructors() {
+		this.name = name
+		this.age = age
+		this.hunger = hunger
+		this.sleepiness = sleepiness
+		this.boredom = boredom
+	}
+
+  }
+
+const game = {
+
+}
+
+
+
