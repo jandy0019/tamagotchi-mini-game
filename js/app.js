78e63d81d608aa5d32e45faceeca7518a0339710
Juice Cx <juicecx@Juices-MacBook-Pro-2.local>
2019-12-19 16:10:59 -0600

    started work on hunger, sleep, boredom

--- js/app.js
+++ js/app.js
@@ -2,32 +2,47 @@ console.log('tamagatchi mini game');
 
 
 
-class Stewie {
-	constructors() {
-		this.name = name
+class Tom {
+	constructors(tommy) {
+		this.name = ''
 		this.age = age
 		this.hunger = hunger
 		this.sleepiness = sleepiness
 		this.boredom = boredom
 	}
-
-  }
+}
 
 const game = {
 	name: '',
-	initiateStewie() {
-
-	},
+	hunger: 10,
+	sleepiness: 10,
+	boredom: 10,
 	startGame() {
-
+		const tom = new Tom()
+		this.startTimer()
 	},
-	checkTimer() {
+	startTimer() {
 		const interval = setInterval(() => {
-
+      	
+    	}, 3000)
+	},
+	stopTimer() {
+		const interval = setInterval(() => {
       	
     	}, 1000)
+	},
+	hunger() {
+
+	},
+	sleepiness() {
+
+	},
+	boredom() {
+
 	}
 }
 
-
+$('#set-name').on('click', () => {
+  game.startGame()
+})
 
